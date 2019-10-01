dc04810b4e11867fcb91503208502a2f8d170c1b
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2019-10-01 22:38:10 +0100

    register feature to import metadata from yaml file

--- cmd/qregister.go
+++ cmd/qregister.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"regexp"
+	"strings"
 
+	"github.com/ghodss/yaml"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/steinarvk/linetool/lib/lines"
@@ -13,14 +17,95 @@ import (
 	orcdeduq "github.com/steinarvk/dedu/module/orc-deduq"
 )
 
+func toString(data interface{}) (string, bool) {
+	switch val := data.(type) {
+	case string:
+		return strings.TrimSpace(val) + "\n", true
+
+	case float32:
+		return fmt.Sprintf("%f", val), true
+	case float64:
+		return fmt.Sprintf("%f", val), true
+
+	case int:
+		return fmt.Sprintf("%d", val), true
+	case int64:
+		return fmt.Sprintf("%d", val), true
+
+	case []byte:
+		return string(val), true
+
+	case bool:
+		if val {
+			return "true\n", true
+		}
+		return "false\n", true
+
+	default:
+		return "", false
+	}
+}
+
+func importMetadataFromFile(entityID, metafile string) error {
+	deduq := orcdeduq.M
+	if deduq.Root == "" {
+		return fmt.Errorf("internal error: orcdeduq was not initialised")
+	}
+
+	data, err := ioutil.ReadFile(metafile)
+	if err != nil {
+		return fmt.Errorf("error reading %q: %v", metafile, err)
+	}
+
+	m := map[string]interface{}{}
+
+	if err := yaml.Unmarshal(data, &m); err != nil {
+		return fmt.Errorf("error parsing %q: %v", metafile, err)
+	}
+
+	pattern := regexp.MustCompile(`^[a-z0-9-]+$`)
+
+	for metaAttrib, value := range m {
+		if !pattern.MatchString(metaAttrib) {
+			return fmt.Errorf("unacceptable metadata attribute name: %q", metaAttrib)
+		}
+
+		metapath := deduq.Filename(entityID, metaAttrib)
+		valueString, ok := toString(value)
+		if !ok {
+			return fmt.Errorf("Unable to convert value %q: %v to string; skipping.", metaAttrib, value)
+		}
+
+		_, err := os.Stat(metapath)
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("error on Stat(%q): %v", metapath, err)
+		}
+		if err == nil {
+			logrus.WithFields(logrus.Fields{
+				"entity_id":     entityID,
+				"metadata_file": metafile,
+				"attrib_file":   metapath,
+			}).Infof("File already exists; skipping.")
+			continue
+		}
+
+		if err := ioutil.WriteFile(metapath, []byte(valueString), 0600); err != nil {
+			return fmt.Errorf("error on WriteFile(%q): %v", metapath, err)
+		}
+	}
+
+	return nil
+}
+
 func init() {
 	var flagVerify bool
+	var flagMetadataFromYAMLSuffixes []string
 
 	var qRegisterCmd = orc.Command(qCmd, orc.Modules(orcdedu.M, orcdeduq.M), cobra.Command{
 		Use:   "register [FILE...]",
 		Short: "Register file(s) as entities",
 	}, func(filenames []string) error {
-		registerOne := func(filename string) error {
+		registerOne := func(filename string) (string, error) {
 			opts := registerOrGetOpts{
 				dedu:         orcdedu.M.Dedu,
 				readonly:     false,
@@ -31,7 +116,7 @@ func init() {
 			if err == nil {
 				fmt.Printf("%s\t%s\n", entityID, filename)
 			}
-			return err
+			return entityID, err
 		}
 
 		// TODO could stream this
@@ -50,8 +135,24 @@ func init() {
 				return fmt.Errorf("file %q does not exist: %v", filename, err)
 			}
 
-			if err := registerOne(filename); err != nil {
+			entityID, err := registerOne(filename)
+			if err != nil {
 				return fmt.Errorf("error registering %q: %v", filename, err)
+			}
+
+			for _, suffix := range flagMetadataFromYAMLSuffixes {
+				metafile := filename + suffix
+				_, err := os.Stat(metafile)
+				if os.IsNotExist(err) {
+					continue
+				}
+				if err != nil {
+					return fmt.Errorf("Stat(%q) returned error: %v", metafile, err)
+				}
+
+				if err := importMetadataFromFile(entityID, metafile); err != nil {
+					return fmt.Errorf("error importing metadata from %q: %v", metafile, err)
+				}
 			}
 		}
 
@@ -59,4 +160,5 @@ func init() {
 	})
 
 	qRegisterCmd.Flags().BoolVar(&flagVerify, "verify", false, "verify every file by re-hashing")
+	qRegisterCmd.Flags().StringSliceVar(&flagMetadataFromYAMLSuffixes, "metadata_yaml_suffix", nil, "create qmfs metadata from adjacent YAML files")
 }
